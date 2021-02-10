a02a3d4d42301bd21f41b7852d70bb8f636182d1
JAY-Kittisak <71874439+JAY-Kittisak@users.noreply.github.com>
2021-02-10 17:17:06 +0700

    ep.6 finished

--- JS-By-Zinglecode/EP-6-Array-find/index.ts
+++ JS-By-Zinglecode/EP-6-Array-find/index.ts
@@ -1,10 +1,13 @@
-interface typeScores {
-  [index: number]: number;
-}
-const scoresF: typeScores = [82, 75, 48, 64, 36];
 
-const failedScore = scoresF.find((score) => {
-  return score < 50;
+const scores: number[] = [82, 75, 48, 64, 36];
+
+const everyone = scores.every((score) => {
+  return score >= 50;
 });
 
-console.log(failedScore);
+console.log(everyone);
+
+
+
+
+
